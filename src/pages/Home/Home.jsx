ae94ae19503434640d8df2b98c4d0195289331f3
Devendra Deore <1002deoredk@gmail.com>
2025-07-29 13:17:57 +0530

    sf

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroSection from "./HeroSec";
 import CountSection from "./CountSection";
 import AboutSection from "./AboutSection";
 import ProjectsSection from "./ProjectsSection";
 import TestimonialsSection from "./TestimonialsSection";
-// import Product from "./Product";
-
+// import PopupForm from "../components/PopupForm"; 
+import PopupForm from "../../components/PopUp/PopupForm";
 function Home() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    // Show popup after 10 seconds
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
       <HeroSection />
@@ -15,6 +26,8 @@ function Home() {
       {/* <Product /> */}
       <ProjectsSection />
       <TestimonialsSection />
+      {/* Popup Form */}
+      <PopupForm isOpen={showPopup} setIsOpen={setShowPopup} />
     </div>
   );
 }
